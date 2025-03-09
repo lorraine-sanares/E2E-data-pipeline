8776db2d3e96a209a6f7a5cecae95b6bf886e87b
Lorraine Sanares <96808637+Raine0554@users.noreply.github.com>
2025-03-09 21:02:23 +1100

    parameterising data pipeline scripts

---
+++ DOCKER_SQL/pipeline.py
@@ -0,0 +1,13 @@
+import sys
+
+import pandas as pd
+
+# command line args that we pass through script
+print(sys.argv)
+
+day = sys.argv[1]
+
+# some fancy stuff with pandas
+
+print(f'job finished successfully for day = {day}')
+
